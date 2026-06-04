0a68f74cb5c83d1fbf33db24110c07fed30b83ff
mdsabbirkhanoni <mdsabbirkhanoni@gmail.com>
2026-06-05 00:10:02 +0600

    config: initialize backend structure and database connection

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/connectDB.js';
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT
+const ORIGIN = process.env.FRONTEND_URL
+
+//cors middleware
+app.use(cors({
+    credentials: true,
+    origin: ORIGIN
+}))
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.get("/", (request, response) => {
+    response.json({
+        message: `Welcome to the 3D VR Application Backend API!, Backend is running on ${PORT}.`
+    })
+})
+
+// First Connect to MongoDB then start the server
+connectDB().then(() => {
+        app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+})
