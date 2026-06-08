d3213bba4e305960246ff5a0774477470e65338f
mdsabbirkhanoni <mdsabbirkhanoni@gmail.com>
2026-06-08 21:56:55 +0600

    fix: differentiate auth flow's public and private routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -9,13 +9,17 @@ import {
     resetPasswordController,
     getMeController
 } from '../controllers/auth.controller.js';
+import { isAuthenticated } from '../middleware/isAuthenticated.js';
 
 const router = Router();
 
+//only authenticated user can access this route
+router.get('/me', isAuthenticated, getMeController);
+
+// Public routes
 router.post('/signup', signUpController);
 router.post('/login', loginController);
 router.post('/logout', logoutController);
-router.get('/me', getMeController);
 router.post('/forgot-password', forgetPasswordRequestController);
 router.post('/verify-otp', verifyOTPController);
 router.post('/reset-password', resetPasswordController);
