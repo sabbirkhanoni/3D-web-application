350776ee4996f04e5e43bf30af43acd13c036e33
mdsabbirkhanoni <mdsabbirkhanoni@gmail.com>
2026-06-10 15:01:25 +0600

    fix: solve incorrect selected object id prop

--- frontend/components/SideBar.jsx
+++ frontend/components/SideBar.jsx
@@ -1,21 +1,21 @@
 import React from "react";
+import { useScene } from "../context/SceneContext";
 import SceneObjectsList from "./dashboard/SceneObjectsList";
 import SelectedObjectPositionDetailsOfSideBar from "./dashboard/SelectedObjectPositionDetailsOfSideBar";
-import { useScene } from "../context/SceneContext";
 
 const SideBar = () => {
-  const { objects, selectedId, selectObject } = useScene();
+  const { objects, selectedObjectId, setSelectedObjectId } = useScene();
 
   return (
     <aside className="w-60 bg-[#070709] border-r border-white/10 flex flex-col shrink-0 overflow-y-auto">
       <SceneObjectsList
         objects={objects}
-        selectedId={selectedId}
-        onSelect={selectObject}
+        selectedId={selectedObjectId}
+        onSelect={setSelectedObjectId}
       />
       <SelectedObjectPositionDetailsOfSideBar
         objects={objects}
-        selectedId={selectedId}
+        selectedId={selectedObjectId}
       />
     </aside>
   );
