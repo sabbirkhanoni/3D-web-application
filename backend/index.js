d89244ddaad89e658f81cd8fd8d4eb7ae6edf634
mdsabbirkhanoni <mdsabbirkhanoni@gmail.com>
2026-06-11 19:29:25 +0600

    refactor: check session in production

--- backend/index.js
+++ backend/index.js
@@ -1,57 +1,61 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import connectDB from './config/connectDB.js';
-import authRoute from './routes/auth.route.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/connectDB.js";
+import authRoute from "./routes/auth.route.js";
 import sceneRoute from "./routes/scene.route.js";
-import session from 'express-session';
+import session from "express-session";
 
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT
-const ORIGIN = process.env.FRONTEND_URL
+const PORT = process.env.PORT;
+const ORIGIN = process.env.FRONTEND_URL;
 
 //cors middleware
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: ORIGIN
-}))
+    origin: ORIGIN,
+  }),
+);
 
 //body parser middleware
-app.use(express.json())
+app.use(express.json());
 //morgan middleware for logging
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 //session middleware
-app.use(session({
-    secret: process.env.SESSION_SECRET || 'default_secret',
+app.set("trust proxy", 1);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-        httpOnly: true, //hide cookie from client-side JavaScript
-        secure: process.env.NODE_ENV === 'production', //use secure cookies in production
-        maxAge: 1000 * 60 * 60 * 24, //cookie expires in 1 day
-        sameSite: 'none' //allow cross-site cookies
-    }
-}));
+      httpOnly: true,
+      secure: true, // always true in Vercel HTTPS
+      sameSite: "none", // cross-site required
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  }),
+);
 
 app.get("/", (request, response) => {
-    response.json({
-        message: `Welcome to the 3D VR Application Backend API!, Backend is running on ${PORT}.`
-    })
-})
+  response.json({
+    message: `Welcome to the 3D VR Application Backend API!, Backend is running on ${PORT}.`,
+  });
+});
 
 // Routes
-app.use('/api/auth', authRoute);
+app.use("/api/auth", authRoute);
 app.use("/api/scene", sceneRoute);
-
-
 
 // First Connect to MongoDB then start the server
 connectDB().then(() => {
-        app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    })
-})
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
